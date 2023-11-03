29890852c55665ab3d0d75a88d825ce7ee829f74
Geronimo Seijo <seijo.geronimo@alu.ing.unlp.edu.ar>
2023-11-03 19:36:26 -0300

    Login y LogOut funcionando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+    
+    def after_sign_in_path_for(_resourse)
+        dashboard_home_path
+    end
+
     def home
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'dashboard/home'
   # Defines the root path route ("/")
-  root "application#home"
+  root 'homepage#home'
+  devise_for :users
 end
